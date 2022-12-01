31b26c6e4dac517880ef1d2542a30c09d587b93d
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2022-12-01 09:04:05 -0500

    Solved day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -29,11 +29,25 @@ def find_max_sub_list(elves: list[list[int]]) -> int:
     return max_sub
 
 
+def sum_sub_lists(elves: list[list[int]]) -> list[int]:
+    new_list = []
+    for elf in elves:
+        sub_sum = sum(elf)
+        new_list.append(sub_sum)
+    return new_list
+
+
+def top_three_elves(elves: list[int]) -> list[int]:
+    elves.sort(reverse=True)
+    return elves[:3]
+
+
 def main():
     data = read_file('input.txt')
     split = split_data(data)
-    max_sub = find_max_sub_list(split)
-    print(f'The elf with the most Calories is carrying {max_sub} Calories')
+    sum_subs = sum_sub_lists(split)
+    top_elves = top_three_elves(sum_subs)
+    print(f'The 3 elves carrying the most Calories are carring {sum(top_elves)} Calories')
 
 
 if __name__ == '__main__':
