f457b027ba63ff01f496b8132ce1465807829c4b
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2022-12-02 11:20:11 -0500

    Refactor and add typing

--- day2/main.py
+++ day2/main.py
@@ -28,8 +28,26 @@ class OutcomeEnums(enum.Enum):
     MUST_WIN = 'Z'
 
 
-def return_needed_outcome(elf_play, needed_outcome):
+def read_file(filename: str) -> list[str]:
+    data = []
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            data.append(line)
+    return data
 
+
+def create_matched_list(data: list[str]) -> list[list[str]]:
+    play_list = []
+    for line in data:
+        sub_list = []
+        plays = line.split(' ')
+        sub_list.append(plays[0])
+        sub_list.append(plays[1].strip())
+        play_list.append(sub_list)
+    return play_list
+
+
+def return_needed_outcome(elf_play: str, needed_outcome: str) -> int:
     if needed_outcome == OutcomeEnums.MUST_LOSE.value:
         if elf_play == ElfEnums.ROCK_PLAY.value:
             return ScoreEnums.SCISSORS_SCORE.value
@@ -53,65 +71,7 @@ def return_needed_outcome(elf_play, needed_outcome):
             return ScoreEnums.ROCK_SCORE.value + WIN_POINTS
 
 
-def will_win(elf_play, santa_play):
-    win = False
-    if elf_play == ElfEnums.ROCK_PLAY.value:
-        if santa_play == SantaEnums.PAPER_PLAY.value:
-            return True
-    elif elf_play == ElfEnums.SCISSORS_PLAY.value:
-        if santa_play == SantaEnums.ROCK_PLAY.value:
-            return True
-    elif elf_play == ElfEnums.PAPER_PLAY.value:
-        if santa_play == SantaEnums.SCISSORS_PLAY.value:
-            return True
-    return win
-
-
-def will_draw(elf_play, santa_play):
-    draw = False
-    if elf_play == ElfEnums.ROCK_PLAY.value and santa_play == SantaEnums.ROCK_PLAY.value:
-        return True
-    elif elf_play == ElfEnums.PAPER_PLAY.value and santa_play == SantaEnums.PAPER_PLAY.value:
-        return True
-    elif elf_play == ElfEnums.SCISSORS_PLAY.value and santa_play == SantaEnums.SCISSORS_PLAY.value:
-        return True
-    return draw
-
-
-def read_file(filename: str) -> list[str]:
-    data = []
-    with open(filename, 'r') as file:
-        for line in file.readlines():
-            data.append(line)
-    return data
-
-
-def create_dict(data):
-    play_list = []
-    for line in data:
-        sub_list = []
-        plays = line.split(' ')
-        sub_list.append(plays[0])
-        sub_list.append(plays[1].strip())
-        play_list.append(sub_list)
-    return play_list
-
-
-def match_enum(enum_val):
-    for val in SantaEnums:
-        if enum_val == val.value:
-            return val
-    for val in ElfEnums:
-        if enum_val == val.value:
-            return val
-
-
-def calculate_score(play_list):
-    score_dict = {
-        SantaEnums.ROCK_PLAY.value: ScoreEnums.ROCK_SCORE.value,
-        SantaEnums.PAPER_PLAY.value: ScoreEnums.PAPER_SCORE.value,
-        SantaEnums.SCISSORS_PLAY.value: ScoreEnums.SCISSORS_SCORE.value
-    }
+def calculate_score(play_list: list[list[str]]) -> int:
     score = 0
     for plays in play_list:
         score += return_needed_outcome(plays[0], plays[1])
@@ -131,8 +91,8 @@ def test():
 def main():
     test()
     data = read_file('input.txt')
-    play_dict = create_dict(data)
-    score = calculate_score(play_dict)
+    play_list = create_matched_list(data)
+    score = calculate_score(play_list)
     print(f'Your score will be {score}')
 
 
