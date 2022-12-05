c977551d2c48e5a33da3d371952b64a12eb4ac5b
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2022-12-05 15:13:42 -0500

    Solved Day 5 Part 2

--- day5/main.py
+++ day5/main.py
@@ -31,6 +31,17 @@ def process_instructions(data, stacks: list[list[str]], stack_input_line_num):
     return stacks
 
 
+def process_instructions_v2(data, stacks: list[list[str]], stack_input_line_num):
+    instrs = data[stack_input_line_num:]
+    search_pattern = r'move\s(?P<num_to_move>\d{1,2})\sfrom\s(?P<move_from>\d{1})\sto\s(?P<move_to>\d{1})'
+    for instr in instrs:
+        nums = re.match(search_pattern, instr)
+        temp_crates = stacks[int(nums.group('move_from'))-1][-int(nums.group('num_to_move')):]
+        del stacks[int(nums.group('move_from'))-1][-int(nums.group('num_to_move')):]
+        stacks[int(nums.group('move_to'))-1].extend(temp_crates)
+    return stacks
+
+
 def get_top_crates(stacks: list[list[str]]):
     return [sub_stack[-1] for sub_stack in stacks]
 
@@ -43,16 +54,16 @@ def test():
                    'move 3 from 1 to 3',
                    'move 2 from 2 to 1',
                    'move 1 from 1 to 2']
-    new_test_stacks = process_instructions(test_instrs, test_stacks, 0)
+    new_test_stacks = process_instructions_v2(test_instrs, test_stacks, 0)
     test_top_crates = get_top_crates(new_test_stacks)
-    assert "".join(test_top_crates) == 'CMZ'
+    assert "".join(test_top_crates) == 'MCD'
 
 
 def main():
     test()
     data = read_file('input.txt')
     stacks = create_stacks()
-    new_stacks = process_instructions(data, stacks, 10)
+    new_stacks = process_instructions_v2(data, stacks, 10)
     top_crates = get_top_crates(new_stacks)
     print(f'The top crates are {"".join(top_crates)}')
 
