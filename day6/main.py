02e3eb92ea4d35d3d0e0fc1d4614b43134293962
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2022-12-06 09:11:40 -0500

    Solve Day 6 Part 2

--- day6/main.py
+++ day6/main.py
@@ -19,14 +19,14 @@ def find_packet_start(data: str, start_message_size: int) -> int:
 
 def test():
     test_data = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
-    test_packet_start = find_packet_start(test_data, 4)
-    assert test_packet_start == 7
+    test_packet_start = find_packet_start(test_data, 14)
+    assert test_packet_start == 19
 
 
 def main():
     test()
     data = read_file('input.txt')
-    marker = find_packet_start(data, 4)
+    marker = find_packet_start(data, 14)
     print(f'{marker} characters need to be processed')
 
 
